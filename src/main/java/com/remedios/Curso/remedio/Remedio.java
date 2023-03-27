ad2f1be02c0d38117b7324a4166f11f16195c55d
Aldo <aldopereira.gyn@gmail.com>
2023-03-27 13:07:01 -0300

    Migration de criação de tabela

--- src/main/java/com/remedios/Curso/remedio/Remedio.java
+++ src/main/java/com/remedios/Curso/remedio/Remedio.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @Entity(name = "remedios")
-@Table(name = "Remedio")
+@Table(name = "Remedios")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,12 +14,16 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EqualsAndHashCode(of = "id")
 public class Remedio {
 
-    public Remedio(DadosCadastroRemedio dados){
+    public Remedio(DadosCadastroRemedio dados) {
         this.nome = dados.nome();
         this.via = dados.via();
         this.lote = dados.lote();
+        this.quantidade = dados.quantidade();
+        this.validade= dados.validade();
         this.laboratorio = dados.laboratorio();
     }
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
