5c0e83d396770aacea3526b2baf762f5f6189fa2
Aldo <aldopereira.gyn@gmail.com>
2023-08-24 10:44:42 -0300

    Personalizando as mensagens de erros para cada campo específico

--- src/main/java/com/remedios/Curso/infra/TratadorDeErros.java
+++ src/main/java/com/remedios/Curso/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package com.remedios.Curso.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +13,20 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> tratador404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> tratador400(MethodArgumentNotValidException ex){
+        var erros =ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(
+                erros.stream().map(DadosErros::new).toList()
+        );
+    }
+
+    public record DadosErros(String campo, String menssagem){
+        public DadosErros(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
