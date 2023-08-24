bfb8461de8d08dfb8bdaa989d7bedd92cc21e28d
Aldo <aldopereira.gyn@gmail.com>
2023-08-24 10:33:21 -0300

    Implementando o RestControllerAdvice

---
+++ src/main/java/com/remedios/Curso/infra/TratadorDeErros.java
@@ -0,0 +1,15 @@
+package com.remedios.Curso.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> tratador404(){
+        return ResponseEntity.notFound().build();
+    }
+}
